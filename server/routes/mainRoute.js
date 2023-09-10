32598b4ba4e28a14669d0bb7c8d894c2388d48c5
112479953+bakare-dev@users.noreply.github.com <112479953+bakare-dev@users.noreply.github.com>
2023-09-10 23:06:25 +0100

    commit

--- server/routes/mainRoute.js
+++ server/routes/mainRoute.js
@@ -9,6 +9,7 @@ router.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.get("/", (req, res) => {
   res.status(200).json({
     message: "Your have reached Bakare Praise Zuri Task 2 Server",
+    endpoints: "/api",
     health: "/health",
     docs: "/swagger",
   });
