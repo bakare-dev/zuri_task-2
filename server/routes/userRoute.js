2201367271f16c9384eaf2164b34623858ddbe3b
112479953+bakare-dev@users.noreply.github.com <112479953+bakare-dev@users.noreply.github.com>
2023-09-10 23:20:25 +0100

    commit

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,6 +2,31 @@ const router = require("express").Router();
 const Controller = require("../../controllers/Controller");
 const controller = new Controller();
 
+router.get("/", (req, res) => {
+    res.status(200).json({
+        getusers: {
+            route: "/users",
+            methods: ["GET"]
+        },
+        createuser: {
+            route: "/user",
+            methods: ["POST"]
+        },
+        getuser: {
+            route: "/user/:key", //key can be name or id
+            methods: ["GET"]
+        },
+        updateuser: {
+            route: "/user/:id",
+            methods: ["PUT"]
+        },
+        deleteuser: {
+            route: "/user/:id",
+            methods: ["DELETE"]
+        }
+    })
+    
+})
 router.get("/users", controller.getUsers);
 router.get("/user/:key", controller.getUser);
 router.post("/user", controller.createUser);
